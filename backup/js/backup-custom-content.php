394bdfac5ab0d4244fd8345552b6b6aa88f2e68c
YesidDaza <u1801475@unimilitar.edu.co>
2016-11-17 12:20:11 -0500

    Se avanzan estilos en información de propuesta

--- backup/js/backup-custom-content.php
+++ backup/js/backup-custom-content.php
@@ -7,7 +7,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-<!--Botones panel izquiero de inicio-->
+<!--Botones panel izquiero de inicio y bloqueo de link temas-->
 <div class="quick-access-links">
 	<div class="activa-link">
 			<a href="http://aprende.colombiaaprende.edu.co/es/activatuciudadania" target="_blank">
@@ -45,6 +45,53 @@
 	    </div>
 	</div>
 </div>
+<script>
+		$( ".curripaz-panel .main-menu .pane-content > .menu > li:last-child > a" ).attr( "href", "javascript:void(0)" );
+</script>
+
+<!--Scripts para estructurar informacion completa de la propuesta-->
+<script>
+		const INFO_CONTAINER = ".curripaz-proposal-container";
+		const FIELD_NAME = "field-name-field-";
+		const SCHOOL_VIEW = "school-info";
+		const TEACHER_VIEW = "teacher-info";
+		const GRADE_TOPIC_VIEW = "grade-topic-info";
+		const DESCRIPTION_VIEW = "description-info";
+		const EXTRA_VIEW = "extra-info";
+		const SCHOOL_DIV = '<div class="col-xs-12 col-sm-7 '+ SCHOOL_VIEW + '"></div>';
+		const TEACHER_DIV = '<div class="col-xs-12 col-sm-5 '+ TEACHER_VIEW + '"></div>';
+		const GRADE_TOPIC_DIV = '<div class="col-xs-12 '+ GRADE_TOPIC_VIEW + '"></div>';
+		const DESCRIPTION_DIV = '<div class="col-xs-12 col-sm-7 '+ DESCRIPTION_VIEW + '"></div>';
+		const EXTRA_DIV = '<div class="col-xs-12 col-sm-5 '+ EXTRA_VIEW + '"></div>';
+
+		if ($( INFO_CONTAINER )) {
+				var infoStructure = '<div>' + SCHOOL_DIV + TEACHER_DIV + '</div>' + '<div>' + GRADE_TOPIC_DIV + '</div>' + '<div>' + DESCRIPTION_DIV + EXTRA_DIV + '</div>';
+				$(infoStructure).insertAfter( $( INFO_CONTAINER + " ." + FIELD_NAME + 'calificacion-cpaz' ) );
+				var infoArray = [
+						[SCHOOL_VIEW, 'nombre-ie'],
+						[SCHOOL_VIEW, 'nombre-sede'],
+						[SCHOOL_VIEW, 'municipios-cp'],
+						[TEACHER_VIEW, 'nombres-del-docente'],
+						[TEACHER_VIEW, 'apellidos-del-docente'],
+						[TEACHER_VIEW, 'correo-de-contacto-cpaz'],
+						[TEACHER_VIEW, 'telefono-de-contacto'],
+						[GRADE_TOPIC_VIEW, 'tema-de-la-propuesta'],
+						[GRADE_TOPIC_VIEW, 'grado-de-la-propuesta'],
+						[DESCRIPTION_VIEW, 'descripcion-cpaz'],
+						[EXTRA_VIEW, 'documento-de-la-propuesta'],
+						[EXTRA_VIEW, 'videos-complementarios'],
+						[EXTRA_VIEW, 'audio-complementario'],
+						[EXTRA_VIEW, 'adjunto-complementario']
+				];
+				for (var i = 0; i < infoArray.length; i++) {
+						putInfoInView(infoArray[i][0], infoArray[i][1]);
+				}
+		}
+
+		function putInfoInView(view, info) {
+				$( INFO_CONTAINER + " ." + view ).append ( $(INFO_CONTAINER + " ." + FIELD_NAME + info) );
+		}
+</script>
 
 <!--Scripts solo para gestor-->
 <script>
